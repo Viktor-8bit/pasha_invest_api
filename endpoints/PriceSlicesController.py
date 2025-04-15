e6a0f80d111fc7d6f95def5bd2494601ec9d3d39
Viktor-8bit <vgssfawfe@list.ru>
2025-04-15 17:58:51 +0700

    created endpoint price slice

--- endpoints/PriceSlicesController.py
+++ endpoints/PriceSlicesController.py
@@ -9,8 +9,8 @@ def price_slice_api_fabrick(priceSliceService: PriceSliceService):
     stock_api = Blueprint('price_slice_api', __name__, template_folder='endpoints')
 
     @stock_api.route("/make_slice_by_figi/")
-    def get_stock_slice():
+    def make_slice_by_figi():
         figi = request.args.get('figi')
-        return priceSliceService. (figi)
+        return priceSliceService.make_slice_by_figi(figi)
 
     return stock_api
--- main.py
+++ main.py
@@ -5,6 +5,7 @@ from dotenv import load_dotenv
 from flask import Flask
 from flask_cors import CORS
 
+from endpoints.PriceSlicesController import price_slice_api_fabrick
 from model import Db
 from services import *
 from repositories import *
@@ -20,12 +21,16 @@ db = Db(connect_str)
 
 # репозитории
 stockRepo = StockRepository(db)
+priceSliceRepository = PriceSlicesRepository(db)
 
 # сервисы
 stockService = StockService(stockRepo)
+priceSliceService = PriceSliceService(priceSliceRepository)
 
 app = Flask(__name__)
 CORS(app, resources={r"/api/*": {"origins": "*"}})
 
 stock_api   = stock_api_fabrick(stockService)
+price_slice_api = price_slice_api_fabrick(priceSliceService)
 app.register_blueprint(stock_api)
+app.register_blueprint(price_slice_api)
